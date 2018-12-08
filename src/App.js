7fe785800612822cf6e118da5370077fb250c728
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-12-07 20:07:22 -0800

    styling added to the intro

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Search from './Search';
 import './App.css';
 
 class App extends Component {
@@ -7,14 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          
           <h2>
             la cocina
           </h2>
-          
+          <Search />
         </header>
       </div>
     );

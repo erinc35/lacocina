c535eafad1b4508db229f9f96b6160ad3c10e765
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-12-16 18:05:57 -0800

    fetchLink onClick event added to all quicklinks

--- src/QuickLinks/QuickLinks.js
+++ src/QuickLinks/QuickLinks.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from "react";
 import bread from '../images/bread.jpeg';
 import cookie from '../images/cookie.jpeg';
@@ -13,7 +12,6 @@ import axios from 'axios';
 let app_id = process.env.REACT_APP_YOUR_APP_ID;
 let app_key = process.env.REACT_APP_YOUR_APP_KEY;
 
-
 class QuickLinks extends Component {
     constructor(props) {
         super(props);
@@ -25,8 +23,9 @@ class QuickLinks extends Component {
 
     fetchQuickLink = e => {
         let ing = e.target.alt;
+        let quick_api = `https://api.edamam.com/search?q=${ing}&app_id=${app_id}&app_key=${app_key}`
         axios
-            .get(`https://api.edamam.com/search?q=${ing}&app_id=${app_id}&app_key=${app_key}`)
+            .get(quick_api)
             .then(response => {
                 console.log(response.data.hits)
                 this.setState({ recipes: response });
@@ -39,32 +38,27 @@ class QuickLinks extends Component {
     render() { 
         return ( 
             <div className='quicklinks'>
-                <div  
-                    href="#" 
-                    className="quicklink"
-                    onClick={this.fetchQuickLink}
-                >
+                <div href="#" className="quicklink" onClick={this.fetchQuickLink}>
                     <img alt='bread' className="icon" src={bread} />
                     <span>Bread Recipes</span>
                 </div>
-                <div  href="#" className="quicklink">
+                <div href="#" className="quicklink" onClick={this.fetchQuickLink}>
                     <img alt='cookie' className="icon" src={cookie} />
                     <span>Cookie Recipes</span>
-                    
                 </div>
-                <div  href="#" className="quicklink">
+                <div href="#" className="quicklink" onClick={this.fetchQuickLink}>
                     <img alt='shrimp' className="icon" src={shrimp} />
                     <span>Shrimp Recipes</span>
                 </div>
-                <div  href="#" className="quicklink">
+                <div href="#" className="quicklink" onClick={this.fetchQuickLink}>
                     <img alt='lamb' className="icon" src={lamb} />
                     <span>Lamb Recipes</span>                    
                 </div>
-                <div href="#" className="quicklink">
+                <div href="#" className="quicklink" onClick={this.fetchQuickLink}>
                     <img alt='salad' className="icon" src={salad} />
                     <span>Salad Recipes</span>
                 </div>
-                <div  href="#" className="quicklink">
+                <div  href="#" className="quicklink" onClick={this.fetchQuickLink}>
                     <img alt='chicken' className="icon" src={chicken} />
                     <span>Chicken Recipes</span>
                 </div>

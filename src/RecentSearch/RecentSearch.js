27ab201b5d5b7b7c97e0abbc8869b25a65573acb
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2019-02-04 21:41:56 -0800

    recent-wrapper wrapped for the recentSearch buttons

--- src/RecentSearch/RecentSearch.js
+++ src/RecentSearch/RecentSearch.js
@@ -10,19 +10,17 @@ let app_key = process.env.REACT_APP_YOUR_APP_KEY;
 class RecentSearch extends Component {
     constructor(props) {
         super(props);
-        
+
     }
     render() { 
         const inputs = this.props.recentSearch;
         return(
-            <div>
                 <div className='recent-wrapper'>
                     {inputs.map((input, ind) => {
                         return <button onClick={(e) => this.props.handleRecentSearch(e,input)} key={ind} className='recent-button'>{input}</button>
                     })}
                 </div>
                 
-            </div>
         )
     }
 }

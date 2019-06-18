776e3c2defce39b38b419c40f46867f1f8c887ce
Erinc Emer <erincemer@gmail.com>
2019-06-17 17:35:17 -0700

    RecentSearch.js changed to stateless component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Search from './Search/Search';
 import Auth from './Auth/Auth';
 import Authenticating from './Auth/Authenticating';
--- src/RecentSearch/RecentSearch.js
+++ src/RecentSearch/RecentSearch.js
@@ -1,28 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import './RecentSearch.css';
-// import axios from 'axios';
 
+const RecentSearch =  props => {
+  
+    const inputs = props.recentSearch;
+    return(
+        <div className='recent-wrapper'>
+            {inputs.map((input, ind) => {
+                return <button onClick={(e) => this.props.handleRecentSearch(e,input)} key={ind} className='recent-button'>{input}</button>
+            })}
+        </div>
+            
+    )
 
-// let app_id = process.env.REACT_APP_YOUR_APP_ID;
-// let app_key = process.env.REACT_APP_YOUR_APP_KEY;
-
-
-class RecentSearch extends Component {
-    constructor(props) {
-        super(props);
-
-    }
-    render() { 
-        const inputs = this.props.recentSearch;
-        return(
-            <div className='recent-wrapper'>
-                {inputs.map((input, ind) => {
-                    return <button onClick={(e) => this.props.handleRecentSearch(e,input)} key={ind} className='recent-button'>{input}</button>
-                })}
-            </div>
-                
-        )
-    }
 }
  
 export default RecentSearch;

674c49a64a61d432fdf6c3037ece49ab10fd2831
Erinc Emer <erincemer@gmail.com>
2019-09-14 13:09:14 -0700

    logo updated on nav

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -90,8 +90,9 @@ class Navigation extends Component {
         return (
             <div>
                 <Navbar light expand="md" className='navbar'>
-                    <NavbarBrand href="/">
+                    <NavbarBrand href="/" className='icon-wrap' >
                         <h1 className="app-title">la cocina</h1>                          
+                        <i class="fas fa-utensils fa-2x icon"></i>
                     </NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
